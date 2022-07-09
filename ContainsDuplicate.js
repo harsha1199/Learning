aad8832075f724593e0a4f30aee829e5eecf3065
Harsha Jorige <saiharsha.jorige@in.pega.com>
2022-07-09 22:57:57 +0530

    Contains Duplicate

---
+++ ContainsDuplicate.js
@@ -0,0 +1,11 @@
+var containsDuplicate = function (nums) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]] !== undefined) {
+      return true;
+    } else {
+      map[nums[i]] = i;
+    }
+  }
+  return false;
+};
